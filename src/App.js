b7d2fbd41612aac054cca2d63b9a9018f00b20a3
Ahae KIM <kimaae@gmail.com>
2023-05-30 17:23:15 +0900

    intersection observer api로 리팩토링

--- src/App.js
+++ src/App.js
@@ -7,45 +7,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasNoMoreResult, setHasNoMoreResult] = useState(false);
-
-  const fetchNextData = () => {
-    if (!hasNoMoreResult) {
-      setPage((page) => page + 1);
-      setIsLoading(true);
-    }
-  };
 
   useEffect(() => {
-    const fetchNextPage = async () => {
-      const scrollY = window.scrollY;
-
+    const fetchData = async () => {
       const { results } = await (
         await fetch(
           `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
         )
       ).json();
-      setMovies([...movies, ...results]);
-
-      if (results.length < 20) setHasNoMoreResult(true);
-      setIsLoading(false);
-      window.scrollTo(0, scrollY);
+      setMovies((movies) => [...movies, ...results]);
     };
-    fetchNextPage();
+    fetchData();
   }, [page]);
 
   return (
     <Routes>
       <Route
         path="/"
-        element={
-          <MovieList
-            data={movies}
-            isLoading={isLoading}
-            fetchNextData={fetchNextData}
-          />
-        }
+        element={<MovieList movies={movies} setPage={setPage} />}
       />
       <Route path="/:id" element={<MovieDetail data={movies} />} />
     </Routes>
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,81 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import MovieCard from "../components/ui/MovieCard";
-import LoadingIndicator from "../components/ui/LoadingIndicator";
 import styles from "./MovieList.module.css";
 import { Link } from "react-router-dom";
 
-function MovieList({ data, isLoading, fetchNextData }) {
-  const handleScroll = () => {
-    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
-
-    // * 첫번째로 했을 때는 더이상 로딩 안되는데 두번째로 하면 로딩 더 됨
-    // const isScrollEnd =
-    //   window.innerHeight + window.scrollY >= document.body.offsetHeight;
-    const isScrollEnd = scrollTop + clientHeight >= scrollHeight;
-
-    // 화면 하단까지 스크롤되었고, !isLoading 이어서 현재 요청 중이 아니라면 다음 페이지 정보 요청
-    if (isScrollEnd && !isLoading) fetchNextData();
-  };
-
-  const throttle = (callback, waits) => {
-    let lastCallback; // timer id of last invocation
-    let lastRan; // time stamp of last invocation
-
-    return (...args) => {
-      const context = this;
-      if (!lastRan) {
-        callback.apply(context, args);
-        lastRan = Date.now();
-      } else {
-        clearTimeout(lastCallback);
-        lastCallback = setTimeout(() => {
-          if (Date.now() - lastRan >= waits) {
-            callback.apply(context, args);
-            lastRan = Date.now();
-          }
-        }, waits - (Date.now() - lastRan));
-      }
-    };
-  };
+function MovieList({ movies, setPage }) {
+  const target = useRef(null);
 
   useEffect(() => {
-    const clientHeight = document.documentElement.clientHeight;
-    const MOVIE_CARD_HEIGHT = 440;
-    const GRID_GAP_HEIGHT = 20;
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setPage((prev) => prev + 1);
+      }
+    });
 
-    // 클라이언트 화면이 영화 카드 20개(10 rows) 이상 들어가고도 남는 경우, 정보를 한 번 더 요청해서 화면을 채우도록 함
-    if (clientHeight > (MOVIE_CARD_HEIGHT + GRID_GAP_HEIGHT) * 10) {
-      fetchNextData();
-    }
+    if (target.current) observer.observe(target.current);
 
-    // 컴포넌트 마운트시, throttle로 0.5초에 한 번씩만 동작하도록 제한
-    window.addEventListener("scroll", throttle(handleScroll, 500));
-
-    // 컴포넌트 언마운트시, scroll 이벤트에 달았던 handleScroll 함수 제거
-    return () => {
-      window.removeEventListener("scroll", throttle(handleScroll, 500));
-    };
-  }, [fetchNextData]);
+    return () => observer.disconnect();
+  }, [movies]);
 
   return (
     <div className={styles.container}>
       <div className={styles.movieListGrid}>
-        {data &&
-          data.map((movie) => (
+        {movies &&
+          movies.map((movie) => (
             <Link to={`/${movie.id}`}>
               <MovieCard
-                key={movie.id}
+                id={movie.id}
                 title={movie.title}
                 posterPath={movie.poster_path}
               />
             </Link>
           ))}
+        {movies.length > 0 && <div ref={target} />}
       </div>
-      {isLoading && (
-        <div className={styles.loadingIndicatorContainer}>
-          <LoadingIndicator />
-        </div>
-      )}
     </div>
   );
 }
